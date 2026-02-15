9065d82b0d068ad7254cc81bcbba001bf216e290
Oliver Calazans <oliver.calazans@gmail.com>
2026-02-15 20:17:47 -0300

    rename HtmlSniffer to UrlSniffer. Add verbose flag

--- stack-sniffer.py
+++ stack-sniffer.py
@@ -11,16 +11,46 @@ from urllib.parse import urlparse, urljoin
 
 @dataclass(slots=True)
 class Data:
-    _lock     : Lock          = Lock()
-    base_url  : str           = None
-    redirect  : bool          = None
-    responses : list[Request] = field(default_factory=list)
-    urls      : set[str]      = field(default_factory=set)
+    _lock      : Lock           = field(default_factory=Lock)
+    responses  : list[Request]  = field(default_factory=list)
+    urls       : set[str]       = field(default_factory=set)
+    known_urls : set[str]       = field(default_factory=set)
+    info       : dict[str, str] = field(default_factory=dict)
+    _base_url  : str            = None
+    redirect   : bool           = None
+    _verbose   : bool           = None
+
+
+    @property
+    def base_url(self) -> str:
+        return self._base_url
+
+
+    @base_url.setter
+    def base_url(self, new_url: str):
+        new_url = new_url.rstrip('/')
+        self._base_url = new_url
+        self.known_urls.add(new_url)
 
 
     def add_response(self, response: Request):
         with self._lock:
             self.responses.append(response)
+    
+
+    def display_progress(self, msg: str):
+        if self._verbose:
+            print(msg)
+    
+
+    def add_url(self, new_url: str):
+        new_url = new_url.rstrip('/')
+
+        if new_url in self.known_urls:
+            return
+
+        self.urls.add(new_url)
+        self.known_urls.add(new_url)
 
 
 
@@ -42,19 +72,24 @@ class StackSniffer:
     
     def analyze(self):
         self._parse_args()
+        self._validate_url()
         self._create_threads_for_requests()
+        self._update_base_url()
         self._analyze_headers()
         self._analyze_htmls()
+        self._display_results()
     
 
 
     def _parse_args(self):
         parser = ArgParser(description='Stack-Sniffer')
         parser.add_argument('--url', type=str, help='URL')
+        parser.add_argument('-v', '--verbose',  action='store_true', help='Display progress')
         parser.add_argument('-r', '--redirect', action='store_true', help='Allow redirection')
         args = parser.parse_args(self._get_args())
 
-        self.data.base_url = self._validate_url(args.url)
+        self.data.base_url = args.url
+        self.data._verbose = args.verbose
         self.data.redirect = args.redirect
 
 
@@ -75,10 +110,13 @@ class StackSniffer:
 
     
 
-    @staticmethod
-    def _validate_url(url: str) -> str:
-        if not url.startswith(('http://', 'https://')):
-            return 'http://' + url
+    def _validate_url(self):
+        if self.data.base_url.startswith(('http://', 'https://')):
+            return
+        
+        incomplited_url    = self.data.base_url
+        self.data.base_url = 'http://' + incomplited_url
+        self.data.display_progress(f'[!] URL changed: {incomplited_url} -> {self.data.base_url}')
 
 
 
@@ -112,70 +150,50 @@ class StackSniffer:
 
         except (Exception, RequestException):
             pass
-
-
     
+
+
+    def _update_base_url(self):
+        if not self.data.redirect:
+            return
+        
+        urls = set()
+        for resp in self.data.responses:
+            urls.add(resp.url)
+        
+        if len(urls) > 1:
+            self._abort(f'Different URL redirections: {urls}')
+
+        self.data.base_url = next(iter(urls))
+        self.data.display_progress(f'[!] Redirected to {self.data.base_url}')
+            
+
+
     def _analyze_headers(self):
         with HeaderSniffer(self.data):
             ... 
     
 
     def _analyze_htmls(self):
-        with HtmlSniffer(self.data):
+        with UrlSniffer(self.data):
             ...
 
-
-
-
-class HeaderSniffer:
     
-    HEADERS_TO_CHECK = [
-        'Server', 'X-Powered-By', 'X-Generator', 'Last-Modified', 'X-AspNet-Version',
-        'X-AspNetMvc-Version', 'X-Runtime', 'X-Frame-Options', 'Location'
-    ]
-
-    __slots__ = ('_info', 'data')
-
-    def __init__(self, data: Data):
-        self.data  : Data = data
-        self._info : dict = {}
-    
-
-
-    def __enter__(self):
-        self._analyze_responses()
-        self._display_results()
-        self._update_url()
-        return self
-
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        return False
-
-
-
-    def _analyze_responses(self):
-        for response in self.data.responses:
-            self._info = { 'status_code': response.status_code }
-            self._analyze_header(response)
-    
-
-
-    def _analyze_header(self, response: Request):
-        for header in self.HEADERS_TO_CHECK:
-            if header in response.headers:
-                self._info[header] = response.headers[header]
-
-
 
     def _display_results(self):
-        bigger  = max(self._info, key=len)
+        self._display_header_info()
+        self._display_links_info()
+    
+
+
+    def _display_header_info(self):
+        bigger  = max(self.data.info, key=len)
         max_len = len(bigger) + 3
 
-        for k, value in self._info.items():
+        for k, value in self.data.info.items():
             space = max_len - len(k)
             desc  = self._format_str(k)
-            print(f'{desc}{space * '.'}: {value}')
+            print(f'[#] {desc}{space * '.'}: {value}')
 
     
 
@@ -186,15 +204,54 @@ class HeaderSniffer:
     
 
 
-    def _update_url(self):
-        if self.data.redirect and 'Location' in self._info:        
-            self.data.base_url = self._info['Location']
+    def _display_links_info(self):
+        print(f'[$] URLs found: {len(self.data.known_urls)}')
+        
+        for i, url in enumerate(self.data.known_urls, start=1):
+            print(f'{i:>3}. {url}')
+
+
+
+
+
+class HeaderSniffer: 
+    
+    HEADERS_TO_CHECK = [
+        'Server', 'X-Powered-By', 'X-Generator', 'Last-Modified', 'X-AspNet-Version',
+        'X-AspNetMvc-Version', 'X-Runtime', 'X-Frame-Options', 'Location'
+    ]
+
+    __slots__ = ('data')
+
+    def __init__(self, data: Data):
+        self.data: Data = data
     
 
 
+    def __enter__(self):
+        self.data.display_progress('[*] Analyzing headers')
+        
+        for response in self.data.responses:
+            self.data.info['status_code'] = response.status_code
+            self._analyze_header(response)
+        
+        return self
 
 
-class HtmlSniffer:
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return False
+
+
+
+    def _analyze_header(self, response: Request):
+        for header in self.HEADERS_TO_CHECK:
+            if header in response.headers:
+                self.data.info[header] = response.headers[header]
+
+
+
+
+class UrlSniffer:
 
     TAGS_AND_ATTRIBUTES = {
         'a': 'href',       'img': 'src',     'link': 'href',
@@ -213,21 +270,18 @@ class HtmlSniffer:
     
 
     def __enter__(self):
-        self._analyze_responses()
-        self._display_results()
+        self.data.display_progress('[*] Analyzing HTMLs')
+        
+        while self.data.responses:
+            response   = self.data.responses.pop() 
+            self._soup = BeautifulSoup(response.text, 'html.parser')
+            self._find_url_in_html()
+        
         return self
 
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         return False
-
-    
-
-    def _analyze_responses(self):
-        while self.data.responses:
-            response   = self.data.responses.pop() 
-            self._soup = BeautifulSoup(response.text, 'html.parser')
-            self._find_url_in_html()
             
     
 
@@ -246,14 +300,7 @@ class HtmlSniffer:
                     continue
                 
                 clean_url = parsed._replace(fragment='').geturl()
-                self.data.urls.add(clean_url)
-    
-
-
-    def _display_results(self):
-        print('\nLinks found')
-        for i, url in enumerate(self.data.urls, start=1):
-            print(f'{i:>3}. {url}')
+                self.data.add_url(clean_url)
 
 
 
